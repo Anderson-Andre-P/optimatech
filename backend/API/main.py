0da18cab365d7a28d2cfbc4403945f895bbcfdab
Anderson-Andre-P <eleu.ander@gmail.com>
2024-10-01 20:48:36 -0300

    refactor: change API endpoints and change data types for db

--- backend/API/main.py
+++ backend/API/main.py
@@ -1,22 +1,24 @@
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Annotated
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
+from datetime import datetime
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 models.Base.metadata.create_all(bind=engine)
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    user_id: int
-
-class UserBase(BaseModel):
-    username: str
 
 def get_db():
     db = SessionLocal()
@@ -31,88 +33,50 @@ db_dependency = Annotated[Session, Depends(get_db)]
 def read_root(db: db_dependency):
     return {"Hello": "World"}
 
-# @app.post('/posts', status_code=status.HTTP_201_CREATED)
-# async def create_post(post: PostBase, db: db_dependency):
-#     db_post = models.Post(**post.dict())
-#     db.add(db_post)
-#     db.commit()
-#     return db_post
-
-# @app.get('/posts/{post_id}', status_code=status.HTTP_200_OK)
-# async def read_post(post_id: int, db: db_dependency):
-#     post = db.query(models.Post).filter(models.Post.id == post_id).first()
-#     if post is None:
-#         raise HTTPException(status_code=404, detail='Post not found')
-#     return post
-
-# @app.delete('/posts/{post_id}', status_code=status.HTTP_200_OK)
-# async def delete_post(post_id: int, db: db_dependency):
-#     db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
-#     if db_post is None:
-#         raise HTTPException(status_code=404, detail='Post not found')
-#     db.delete(db_post)
-#     db.commit()
-#     return {"message": "Post deleted"}
-
-# @app.post('/users/', status_code=status.HTTP_201_CREATED)
-# async def create_user(user: UserBase, db: db_dependency):
-#     db_user = models.User(username=user.username)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
-
-# @app.get('/users/{user_id}', status_code=status.HTTP_200_OK)
-# async def read_user(user_id: int, db: db_dependency):
-#     user = db.query(models.User).filter(models.User.id == user_id).first()
-#     if user is None:
-#         raise HTTPException(status_code=404, detail='User not found')
-#     return user
-
-
 class MultijatoDataResponse(BaseModel):
-    temperatura_entrada_multijato: str
-    temperatura_saida_multijato: str
+    tempo: datetime
+    temperatura_entrada_multijato: float
+    temperatura_saida_multijato: float
 
-    abertura_valvula_alimentacao: str
-    nivel_cozedor: str
-    capacitancia_cozedor: str
-    pressao_cozedor: str
+    abertura_valvula_alimentacao: float
+    nivel_cozedor: float
+    capacitancia_cozedor: float
+    pressao_cozedor: float
 
-    abertura_valvula_alimentacao_0: str
-    nivel_cozedor_0: str
-    capacitancia_cozedor_0: str
-    pressao_cozedor_0: str
+    abertura_valvula_alimentacao_0: float
+    nivel_cozedor_0: float
+    capacitancia_cozedor_0: float
+    pressao_cozedor_0: float
 
-    abertura_valvula_alimentacao_1: str
-    nivel_cozedor_1: str
-    capacitancia_cozedor_1: str
-    pressao_cozedor_1: str
+    abertura_valvula_alimentacao_1: float
+    nivel_cozedor_1: float
+    capacitancia_cozedor_1: float
+    pressao_cozedor_1: float
 
-    abertura_valvula_alimentacao_2: str
-    nivel_cozedor_2: str
-    capacitancia_cozedor_2: str
-    pressao_cozedor_2: str
+    abertura_valvula_alimentacao_2: float
+    nivel_cozedor_2: float
+    capacitancia_cozedor_2: float
+    pressao_cozedor_2: float
 
-    abertura_valvula_alimentacao_3: str
-    nivel_cozedor_3: str
-    capacitancia_cozedor_3: str
-    pressao_cozedor_3: str
+    abertura_valvula_alimentacao_3: float
+    nivel_cozedor_3: float
+    capacitancia_cozedor_3: float
+    pressao_cozedor_3: float
 
-    abertura_valvula_alimentacao_4: str
-    nivel_cozedor_4: str
-    capacitancia_cozedor_4: str
-    pressao_cozedor_4: str
+    abertura_valvula_alimentacao_4: float
+    nivel_cozedor_4: float
+    capacitancia_cozedor_4: float
+    pressao_cozedor_4: float
 
-    abertura_valvula_alimentacao_5: str
-    nivel_cozedor_5: str
-    capacitancia_cozedor_5: str
-    pressao_cozedor_5: str
+    abertura_valvula_alimentacao_5: float
+    nivel_cozedor_5: float
+    capacitancia_cozedor_5: float
+    pressao_cozedor_5: float
 
-    abertura_valvula_alimentacao_6: str
-    nivel_cozedor_6: str
-    capacitancia_cozedor_6: str
-    pressao_cozedor_6: str
+    abertura_valvula_alimentacao_6: float
+    nivel_cozedor_6: float
+    capacitancia_cozedor_6: float
+    pressao_cozedor_6: float
 
     class Config:
         orm_mode = True
--- backend/API/models.py
+++ backend/API/models.py
@@ -1,64 +1,50 @@
-from sqlalchemy import Boolean, Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Float, DateTime
 from database import Base
-
-# class User(Base):
-#     __tablename__ = 'users'
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     username = Column(String(50))
-
-# class Post(Base):
-#     __tablename__ = 'posts'
-
-#     id  = Column(Integer, primary_key=True, index=True)
-#     title = Column(String(40))
-#     content = Column(String(40))
-#     user_id = Column(Integer)
 
 class ProcessData(Base):
     __tablename__ = 'process_data'
 
-    tempo = Column("Tempo", DateTime,  primary_key=True, index=True)
+    tempo = Column("Tempo", DateTime, primary_key=True, index=True)
 
-    temperatura_entrada_multijato = Column("Temperatura de Entrada do Multijato", String)
-    temperatura_saida_multijato = Column("Temperatura de saída no multijato", String)
+    temperatura_entrada_multijato = Column("Temperatura de Entrada do Multijato", Float)
+    temperatura_saida_multijato = Column("Temperatura de saída no multijato", Float)
 
-    abertura_valvula_alimentacao = Column("Abertura da válvula de alimentação", String)
-    nivel_cozedor = Column("Nível do cozedor", String)
-    capacitancia_cozedor = Column("Capacitância do cozedor", String)
-    pressao_cozedor = Column("Pressão do cozedor", String)
+    abertura_valvula_alimentacao = Column("Abertura da válvula de alimentação", Float)
+    nivel_cozedor = Column("Nível do cozedor", Float)
+    capacitancia_cozedor = Column("Capacitância do cozedor", Float)
+    pressao_cozedor = Column("Pressão do cozedor", Float)
 
-    abertura_valvula_alimentacao_0 = Column("Abertura da válvula de alimentação_[0]", String)
-    nivel_cozedor_0 = Column("Nível do cozedor_[0]", String)
-    capacitancia_cozedor_0 = Column("Capacitância do cozedor_[0]", String)
-    pressao_cozedor_0 = Column("Pressão do cozedor_[0]", String)
+    abertura_valvula_alimentacao_0 = Column("Abertura da válvula de alimentação.1", Float)
+    nivel_cozedor_0 = Column("Nível do cozedor.1", Float)
+    capacitancia_cozedor_0 = Column("Capacitância do cozedor.1", Float)
+    pressao_cozedor_0 = Column("Pressão do cozedor.1", Float)
 
-    abertura_valvula_alimentacao_1 = Column("Abertura da válvula de alimentação_[1]", String)
-    nivel_cozedor_1 = Column("Nível do cozedor_[1]", String)
-    capacitancia_cozedor_1 = Column("Capacitância do cozedor_[1]", String)
-    pressao_cozedor_1 = Column("Pressão do cozedor_[1]", String)
+    abertura_valvula_alimentacao_1 = Column("Abertura da válvula de alimentação.2", Float)
+    nivel_cozedor_1 = Column("Nível do cozedor.2", Float)
+    capacitancia_cozedor_1 = Column("Capacitância do cozedor.2", Float)
+    pressao_cozedor_1 = Column("Pressão do cozedor.2", Float)
 
-    abertura_valvula_alimentacao_2 = Column("Abertura da válvula de alimentação_[2]", String)
-    nivel_cozedor_2 = Column("Nível do cozedor_[2]", String)
-    capacitancia_cozedor_2 = Column("Capacitância do cozedor_[2]", String)
-    pressao_cozedor_2 = Column("Pressão do cozedor_[2]", String)
+    abertura_valvula_alimentacao_2 = Column("Abertura da válvula de alimentação.3", Float)
+    nivel_cozedor_2 = Column("Nível do cozedor.3", Float)
+    capacitancia_cozedor_2 = Column("Capacitância do cozedor.3", Float)
+    pressao_cozedor_2 = Column("Pressão do cozedor.3", Float)
 
-    abertura_valvula_alimentacao_3 = Column("Abertura da válvula de alimentação_[3]", String)
-    nivel_cozedor_3 = Column("Nível do cozedor_[3]", String)
-    capacitancia_cozedor_3 = Column("Capacitância do cozedor_[3]", String)
-    pressao_cozedor_3 = Column("Pressão do cozedor_[3]", String)
+    abertura_valvula_alimentacao_3 = Column("Abertura da válvula de alimentação.4", Float)
+    nivel_cozedor_3 = Column("Nível do cozedor.4", Float)
+    capacitancia_cozedor_3 = Column("Capacitância do cozedor.4", Float)
+    pressao_cozedor_3 = Column("Pressão do cozedor.4", Float)
 
-    abertura_valvula_alimentacao_4 = Column("Abertura da válvula de alimentação_[4]", String)
-    nivel_cozedor_4 = Column("Nível do cozedor_[4]", String)
-    capacitancia_cozedor_4 = Column("Capacitância do cozedor_[4]", String)
-    pressao_cozedor_4 = Column("Pressão do cozedor_[4]", String)
+    abertura_valvula_alimentacao_4 = Column("Abertura da válvula de alimentação.5", Float)
+    nivel_cozedor_4 = Column("Nível do cozedor.5", Float)
+    capacitancia_cozedor_4 = Column("Capacitância do cozedor.5", Float)
+    pressao_cozedor_4 = Column("Pressão do cozedor.5", Float)
 
-    abertura_valvula_alimentacao_5 = Column("Abertura da válvula de alimentação_[5]", String)
-    nivel_cozedor_5 = Column("Nível do cozedor_[5]", String)
-    capacitancia_cozedor_5 = Column("Capacitância do cozedor_[5]", String)
-    pressao_cozedor_5 = Column("Pressão do cozedor_[5]", String)
+    abertura_valvula_alimentacao_5 = Column("Abertura da válvula de alimentação.6", Float)
+    nivel_cozedor_5 = Column("Nível do cozedor.6", Float)
+    capacitancia_cozedor_5 = Column("Capacitância do cozedor.6", Float)
+    pressao_cozedor_5 = Column("Pressão do cozedor.6", Float)
 
-    abertura_valvula_alimentacao_6 = Column("Abertura da válvula de alimentação_[6]", String)
-    nivel_cozedor_6 = Column("Nível do cozedor_[6]", String)
-    capacitancia_cozedor_6 = Column("Capacitância do cozedor_[6]", String)
-    pressao_cozedor_6 = Column("Pressão do cozedor_[6]", String)
+    abertura_valvula_alimentacao_6 = Column("Abertura da válvula de alimentação.7", Float)
+    nivel_cozedor_6 = Column("Nível do cozedor.7", Float)
+    capacitancia_cozedor_6 = Column("Capacitância do cozedor.7", Float)
+    pressao_cozedor_6 = Column("Pressão do cozedor.7", Float)
